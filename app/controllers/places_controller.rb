ba0219f15714f2de5443d7066f241d4bf3eac04e
farhanprasla10 <farhan.prasla31@gmil.com>
2019-03-10 15:00:41 +0000

    smiple form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,18 +1,17 @@
 class PlacesController < ApplicationController
-
   def index
     @places = Place.all
   end
-  
+
   def new
-  @place = Place.new
+    @place = Place.new
   end
-  
+
   def create
-  Place.create(place_params)
-  redirect_to root_path
+    Place.create(place_params)
+    redirect_to root_path
   end
-  
+
   private
 
   def place_params
