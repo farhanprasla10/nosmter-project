4685a36e365ac51c82a09bb8c9a5ec795b271e84
farhanprasla10 <farhan.prasla31@gmil.com>
2019-03-19 19:29:24 +0000

    geocoder

--- Gemfile
+++ Gemfile
@@ -77,3 +77,7 @@ gem "font-awesome-rails"
 
 gem 'devise'
 
+gem "geocoder"
+
+gem "figaro"
+
---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,5 @@
+Geocoder.configure(
+  lookup: :google,
+  api_key: ENV['AIzaSyD7In-uWaFcLn7gHMCYIiYD8jF0LioQdD8'],
+  timeout: 15
+)
