559d3cea2cb3415bc8ffa5bea982bbbe928ea407
farhanprasla10 <farhan.prasla31@gmil.com>
2019-03-30 18:15:54 +0000

    migeraton error

--- db/migrate/20190323232605_create_photos.rb
+++ db/migrate/20190323232605_create_photos.rb
@@ -1,7 +1,7 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
-    
+      t.string :picture
       t.integer :user_id
       t.integer :place_id
 
